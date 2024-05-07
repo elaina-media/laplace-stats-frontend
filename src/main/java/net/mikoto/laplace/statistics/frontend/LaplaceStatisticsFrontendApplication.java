915bb343c086abcec6c802923a6f004f8963a76c
mikoto <mikoto@ungine.cn>
2024-05-07 15:35:19 +0800

    :tada: init

---
+++ src/main/java/net/mikoto/laplace/statistics/frontend/LaplaceStatisticsFrontendApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.laplace.statistics.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaplaceStatisticsFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaplaceStatisticsFrontendApplication.class, args);
+    }
+
+}
