e60e9c4e5adda06df9adae2dcbcfb7f9ae064a86
mikoto <mikoto@ungine.cn>
2024-05-16 01:57:40 +0800

    :rocket: 使用tailwindcss可执行文件进行构建 & 完成nav栏设计

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      './src/index.html'
+  ],
   theme: {
+    colors: {
+      'orange': '#e55829',
+      'orange-light': '#db6f39',
+      'white': '#e9dccd',
+      'dark-gray': '#616161',
+    },
     extend: {},
   },
   plugins: [],
