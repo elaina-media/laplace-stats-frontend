3cc667ae0b126a61263a424b3501c26266485de6
mikoto <mikoto@ungine.cn>
2024-05-15 12:39:12 +0800

    :heavy_plus_sign: 引入tailwindcss

--- src/test/java/net/mikoto/laplace/statistics/frontend/LaplaceStatisticsFrontendApplicationTests.java
+++ src/test/java/net/mikoto/laplace/statistics/frontend/LaplaceStatisticsFrontendApplicationTests.java
@@ -1,9 +1,7 @@
 package net.mikoto.laplace.statistics.frontend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LaplaceStatisticsFrontendApplicationTests {
 
     @Test
